6841a5c477ca67bde7c7769269ae32aa8b249203
格斗家不爱在外太空沉思 <1796308503@qq.com>
2024-05-17 18:05:55 +0800

    update

---
+++ 01-环境搭建/03-结合react开发/threejsreact/src/03-旋转缩放.jsx
@@ -0,0 +1,78 @@
+import './App.css'
+import * as THREE from 'three'
+// 导入轨道控制器
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+
+function App() {
+	// 创建场景
+	const scene = new THREE.Scene()
+
+	// 创建相机                                // 视角fov // 宽高比 // 近平面 // 远平面
+	const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+	// 创建渲染器
+	const render = new THREE.WebGLRenderer()
+	render.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(render.domElement)
+
+	// 创建几何体
+	const geometry = new THREE.BoxGeometry(1, 1, 1)
+	// 创建材质
+	const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+	const parentMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+	// 创建网格
+	let parentCube = new THREE.Mesh(geometry, parentMaterial)
+	const cube = new THREE.Mesh(geometry, material)
+	parentCube.add(cube)
+	parentCube.position.set(-3, 0, 0)
+	// 父元素缩放子元素也跟着缩放，然后才是子元素的缩放
+	// parentCube.scale.set(2, 2, 2)
+	// 父元素旋转子元素也跟着旋转，然后才是子元素的旋转
+	parentCube.rotation.x = Math.PI / 4
+
+	// 设置网格位置
+	// cube.position.x = 2
+	// cube.position.y = 4
+	// cube.position.z = 4
+	cube.position.set(3, 0, 0)
+	// 设置立方体的放大
+	cube.scale.set(2, 2, 2)
+	// 绕着x轴旋转
+	cube.rotation.x = Math.PI / 4
+
+	// 将网格添加到场景中
+	scene.add(parentCube)
+
+	// 设置相机位置
+	camera.position.z = 2
+	camera.position.y = 4
+	camera.position.x = 4
+
+	camera.lookAt(0, 0, 0)
+
+	// 添加世界坐标辅助器
+	const axesHelper = new THREE.AxesHelper(2)
+	scene.add(axesHelper)
+
+	// 添加轨道控制器
+	const controls = new OrbitControls(camera, render.domElement)
+	// 设置带阻尼惯性
+	controls.enableDamping = true
+	// 设置阻尼系数
+	controls.dampingFactor = 0.15
+	// 设置自动旋转
+	controls.autoRotate = false
+
+	// 渲染
+	function animate() {
+		controls.update()
+		requestAnimationFrame(animate)
+		// cube.rotation.x += 0.01
+		// cube.rotation.y += 0.01
+		render.render(scene, camera)
+	}
+	animate()
+	return <></>
+}
+
+export default App
--- 01-环境搭建/03-结合react开发/threejsreact/src/04-响应式画布与全屏控制.jsx
+++ 01-环境搭建/03-结合react开发/threejsreact/src/04-响应式画布与全屏控制.jsx
@@ -2,48 +2,73 @@ import './App.css'
 import * as THREE from 'three'
 // 导入轨道控制器
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
-// 导入lil.gui
-import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
 
 function App() {
+	// 创建场景
 	const scene = new THREE.Scene()
+
+	// 创建相机                                // 视角fov // 宽高比 // 近平面 // 远平面
 	const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+	// 创建渲染器
 	const render = new THREE.WebGLRenderer()
 	render.setSize(window.innerWidth, window.innerHeight)
 	document.body.appendChild(render.domElement)
 
+	// 创建几何体
 	const geometry = new THREE.BoxGeometry(1, 1, 1)
+	// 创建材质
 	const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 	const parentMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-	// 设置父元素材质为线框
-	parentMaterial.wireframe = true
+	// 创建网格
 	let parentCube = new THREE.Mesh(geometry, parentMaterial)
 	const cube = new THREE.Mesh(geometry, material)
 	parentCube.add(cube)
 	parentCube.position.set(-3, 0, 0)
+	// 父元素缩放子元素也跟着缩放，然后才是子元素的缩放
+	// parentCube.scale.set(2, 2, 2)
+	// 父元素旋转子元素也跟着旋转，然后才是子元素的旋转
 	parentCube.rotation.x = Math.PI / 4
+
+	// 设置网格位置
+	// cube.position.x = 2
+	// cube.position.y = 4
+	// cube.position.z = 4
 	cube.position.set(3, 0, 0)
+	// 设置立方体的放大
 	cube.scale.set(2, 2, 2)
+	// 绕着x轴旋转
 	cube.rotation.x = Math.PI / 4
 
+	// 将网格添加到场景中
 	scene.add(parentCube)
 
+	// 设置相机位置
 	camera.position.z = 2
 	camera.position.y = 4
 	camera.position.x = 4
+
 	camera.lookAt(0, 0, 0)
 
+	// 添加世界坐标辅助器
 	const axesHelper = new THREE.AxesHelper(2)
 	scene.add(axesHelper)
 
+	// 添加轨道控制器
 	const controls = new OrbitControls(camera, render.domElement)
+	// 设置带阻尼惯性
 	controls.enableDamping = true
+	// 设置阻尼系数
 	controls.dampingFactor = 0.15
+	// 设置自动旋转
 	controls.autoRotate = false
 
+	// 渲染
 	function animate() {
 		controls.update()
 		requestAnimationFrame(animate)
+		// cube.rotation.x += 0.01
+		// cube.rotation.y += 0.01
 		render.render(scene, camera)
 	}
 	animate()
@@ -57,58 +82,26 @@ function App() {
 		camera.updateProjectionMatrix()
 	})
 
-	let eventObj = {
-		Fullscreen: function () {
-			document.body.requestFullscreen()
-		},
-		ExitFullscreen: function () {
-			document.exitFullscreen()
-		},
+	const fullScreen = () => {
+		// render.domElement.requestFullscreen()
+		document.body.requestFullscreen()
 	}
 
-	// 创建GUI
-	const gui = new GUI()
-	gui.add(eventObj, 'Fullscreen')
-	gui.add(eventObj, 'ExitFullscreen')
-	// 控制立方体位置
-	// gui.add(cube.position, 'x', -5, 5).name('立方体x轴位置')
-	let folder = gui.addFolder('立方体位置')
-	folder
-		.add(cube.position, 'x')
-		.min(-5)
-		.max(5)
-		.step(0.1)
-		.name('立方体x轴位置')
-		.onChange((val) => console.log(val))
-		.onFinishChange(() => console.log('finish'))
-	folder
-		.add(cube.position, 'y')
-		.min(-5)
-		.max(5)
-		.step(0.1)
-		.name('立方体y轴位置')
-		.onChange((val) => console.log(val))
-		.onFinishChange(() => console.log('finish'))
-	folder
-		.add(cube.position, 'z')
-		.min(-5)
-		.max(5)
-		.step(0.1)
-		.name('立方体z轴位置')
-		.onChange((val) => console.log(val))
-		.onFinishChange(() => console.log('finish'))
-	gui.add(parentMaterial, 'wireframe').name('父元素线框模式')
-
-	let colorParams = {
-		cubeColor: '#ff0000',
+	const exitfullScreen = () => {
+		document.exitFullscreen()
 	}
-	gui
-		.add(colorParams, 'cubeColor')
-		.name('立方体颜色')
-		.onChange((val) => {
-			cube.material.color.set(val)
-		})
-	return <></>
+
+	return (
+		<>
+			<div style={{ color: '#ffffff' }} onClick={fullScreen}>
+				点击全屏
+			</div>
+
+			<div style={{ color: '#ffffff' }} onClick={exitfullScreen}>
+				退出全屏
+			</div>
+		</>
+	)
 }
 
 export default App
